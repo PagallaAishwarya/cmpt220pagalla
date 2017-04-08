6d9dbed4889c2bb58d6bd7fc34b072f6abcab5a2
Juan Arias <juan.arias@marist.edu>
2017-04-08 12:18:17 -0400

    Feedback on Lab 9

---
+++ labs/9/ASH1203.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab9;
+
+/**
+ *
+ * @author aishw
+ */
+import java.util.Scanner;
+public class ASH1203 {
+
+    public static void main(String[] args) {
+
+        // creating an array with 100 random integers
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an index: ");
+        try {
+            int index = input.nextInt();
+            System.out.println("array[" + index + "] = " + array[index]);
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("INDEX " + ex.getLocalizedMessage() + " is out of bounds");
+        } catch (Exception ex) {
+            System.out.println("INVALID INPUT");
+        }
+    }
+    
+}
